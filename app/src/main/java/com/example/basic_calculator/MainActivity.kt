dfbdffd57d3fe3490ebff0aeb1916dc0f9c1c99b
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-10 13:52:26 -0800

    Created multiple linear layouts that run horizontally and added 5 rows of buttons

--- app/src/main/java/com/example/basic_calculator/MainActivity.kt
+++ app/src/main/java/com/example/basic_calculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.basic_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view:View)
+    {
+        Toast.makeText(this,"Button Pressed", Toast.LENGTH_SHORT).show()
     }
 }
